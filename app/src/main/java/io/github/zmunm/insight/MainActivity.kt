a09c9fa0512543b758599572f362e94d8bc10285
zmunm <atheist2197@naver.com>
2020-12-15 19:44:41 +0900

    color

--- app/src/main/java/io/github/zmunm/insight/MainActivity.kt
+++ app/src/main/java/io/github/zmunm/insight/MainActivity.kt
@@ -1,13 +1,45 @@
 package io.github.zmunm.insight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.*
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = Bitmap.createBitmap(1080, 1080, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(bitmap)
+
+        val pnt = Paint()
+        pnt.isAntiAlias = true
+
+
+        val gradient =
+            LinearGradient(0f, 0f, 0f, 1080f, Color.CYAN, Color.MAGENTA, Shader.TileMode.CLAMP)
+        val sat = LinearGradient(
+            0f,
+            0f,
+            1080f,
+            0f,
+            Color.TRANSPARENT,
+            Color.YELLOW,
+            Shader.TileMode.CLAMP
+        )
+
+        val merged = ComposeShader(gradient, sat, PorterDuff.Mode.MULTIPLY)
+        pnt.shader = merged
+        canvas.drawRect(RectF(0f, 0f, 1080f, 1080f), pnt)
+
+        findViewById<ImageView>(R.id.image).apply {
+            setImageBitmap(bitmap)
+        }
     }
 }
