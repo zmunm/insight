6978d4063de57869bed091a2ef35b6ac9e1dffb1
zmunm <atheist2197@naver.com>
2021-06-12 17:00:00 +0900

    Add BaseRealmDao

--- cache/realm/src/main/java/io/github/zmunm/insight/cache/dao/BaseRealmDao.kt
+++ cache/realm/src/main/java/io/github/zmunm/insight/cache/dao/BaseRealmDao.kt
@@ -1,4 +1,27 @@
 package io.github.zmunm.insight.cache.dao
 
-class BaseRealmDao {
+import io.realm.Realm
+
+internal interface BaseRealmDao {
+    fun <R> asyncRealm(run: (Realm) -> R) =
+        Realm.getDefaultInstance().use {
+            it.executeTransactionAsync { realm ->
+                run(realm)
+            }
+        }
+
+    fun <R> instantRealmTransaction(run: (Realm) -> R) =
+        Realm.getDefaultInstance().use { realm ->
+            realm.beginTransaction()
+            try {
+                run(realm)
+            } finally {
+                realm.commitTransaction()
+            }
+        }
+
+    fun <R> instantRealm(run: (Realm) -> R) =
+        Realm.getDefaultInstance().use { realm ->
+            run(realm)
+        }
 }
